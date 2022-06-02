1d52a95d0c1bbf9aeb2144811becaf1ba59ced9f
sulfranc <sulfranc@gmail.com>
2022-06-02 17:52:52 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import Nav from "./Nav";
+import hogs from "../porkers_data"
+import HogContainer from "./HogContainer.js"
 
-import hogs from "../porkers_data";
+function App (){
 
-function App() {
 	return (
 		<div className="App">
 			<Nav />
+			<HogContainer hogs={hogs} />
 		</div>
-	);
+		
+
+	)
 }
 
-export default App;
+export default App 
